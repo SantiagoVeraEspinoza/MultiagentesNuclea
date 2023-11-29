b755be1516e6f9d35a7df2acac5f7981915d1e9d
Santiago Vera Espinoza <santiveraespinoza@gmail.com>
2023-11-28 20:21:47 -0600

    Added Unreal Link with Python

---
+++ ConnectionPython/unreal_link.py
@@ -0,0 +1,31 @@
+class Pos:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+LOAD_POS = Pos(-31210.0, 22570.0)
+
+class truck:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+def READ_SPEED_FUNCTION():
+    speed = 1000
+    return speed
+
+def see(pos_x, pos_y):
+    truck_agent = truck(pos_x, pos_y)
+    args = {0:truck_agent}
+    return args
+
+def move():
+    return
+
+def main(pos_x, pos_y):
+    args = see(pos_x, pos_y)
+    move()
+    return
